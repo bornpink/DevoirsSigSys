067155ee00359aa63af528866c8be7073a29ab24
Sacha <103340054+bornpink@users.noreply.github.com>
2023-03-08 18:31:05 +0100

    Update devoir3.py

--- devoir3.py
+++ devoir3.py
@@ -1,20 +1,28 @@
+## LE CONTENU DE CETTE CELLLULE EST A SOUMETTRE SUR INGINIOUS
 def systemeDiscret(A,B,C,D,ts,x=None):
     """
     Calcule la réponse d'un système discret en fonction d'une entrée x optionnelle.
     Si x n'est pas fourni, la fonction renvoie la réponse impulsionnelle du système.
 
     Le système est décrit par un temps d'échantillonnage ts > 0 en secondes,
-    ainsi que par les matrices A,B,C,D telles que :
-    q[n+1] = A*q[n] + B*x[n],
-    y[n] = C*q[n] + D*x[n].
+    et par les matrices A,B,C,D qui sont telles que :
+        q[n+1] = A*q[n] + B*x[n],
+        y[n] = C*q[n] + D*x[n].
     
     Arguments
     ---------
-    TODO
-    
+    A (np.ndarray): matrice d'état du système (cf. définition ci-dessus)
+    B (np.ndarray): matrice de commande du système (cf. définition ci-dessus)
+    C (np.ndarray): matrice d'observation du système (cf. définition ci-dessus)
+    D (np.ndarray): matrice d'action directe du système (cf. définition ci-dessus)
+    ts (float): temps d'échantillonnage en secondes, doit être >0
+    x (np.ndarray)(optionnel): entrée du système, qui commence en t=0
+
     Retourne
     --------
-    TODO
+    ty (np.ndarray): vecteur temps associé au signal y
+    y (np.ndarray): réponse du système y pour l'entrée x, ou réponse impulsionnelle si x n'est pas fourni
+
     """
     
     # Représentation du système
@@ -32,17 +40,45 @@ def systemeDiscret(A,B,C,D,ts,x=None):
     y = np.array(y).squeeze()
     
     return (ty, y)
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+## LE CONTENU DE CETTE CELLLULE EST A SOUMETTRE SUR INGINIOUS
 def computeMatrices(alpha,beta):
+    """
+    Renvoie la représentation d'état du système y[n] = alpha*x[n] + beta*y[n-1].
+    Pour rappel, les matrices A,B,C,D sont telles que :
+        q[n+1] = A*q[n] + B*x[n], 
+        y[n] = C*q[n] + D*x[n].
     
-    A = 0 #TODO
-    B = 0 #TODO
-    C = 0 #TODO
-    D = 0 #TODO
+    Arguments
+    ---------
+    alpha (float): paramètre du système
+    beta (float): paramètre du système
+
+    Retourne
+    ---------
+    A (np.ndarray): matrice d'état du système (cf. définition ci-dessus)
+    B (np.ndarray): matrice de commande du système (cf. définition ci-dessus)
+    C (np.ndarray): matrice d'observation du système (cf. définition ci-dessus)
+    D (np.ndarray): matrice d'action directe du système (cf. définition ci-dessus)
+    """
+    
+    # q[n+1] = beta*q[n] + alpha*x[n]
+    # y[n]   = beta*q[n] + alpha*x[n]
+
+    A = np.array(beta)
+    B = np.array(alpha)
+    C = np.array(beta)
+    D = np.array(alpha)
     
     return (A,B,C,D)
